56911cd4197594bda6fd5c03ec9c3a2d597f2d0e
Deyvid Popov <47706857+DeyvidBG@users.noreply.github.com>
2023-01-10 10:20:37 +0200

    Update\

---
+++ src/dao/SchoolRepository.ts
@@ -0,0 +1,48 @@
+import { Identifiable } from '../model/shared-types'
+import { IRepository } from './shared-types'
+import { tryCatchWrapper } from '../utils'
+import { BaseRepository } from '.'
+
+export interface ISchoolRepository<K, V extends Identifiable<K>>
+  extends IRepository<K, V> {}
+
+class SchoolRepository<K, V extends Identifiable<K>>
+  extends BaseRepository<K, V>
+  implements ISchoolRepository<K, V>
+{
+  constructor() {
+    super()
+  }
+
+  private static readonly CREATE_QUERY =
+    'INSERT INTO schools (id, name, welcome_text, country, city, zip_code, street_address, website) VALUES (?,?,?,?,?,?,?)'
+  private static readonly GET_ALL_QUERY =
+    'SELECT id, name, welcome_text AS welcomeText, principal_id AS principalId, vice_principal_id AS vicePrincipalId, country, city, zip_code as zipCode, street_address AS streetAddress, website FROM schools'
+  private static readonly GET_BY_ID =
+    'SELECT id, name, welcome_text AS welcomeText, principal_id AS principalId, vice_principal_id AS vicePrincipalId, country, city, zip_code as zipCode, street_address AS streetAddress, website FROM schools WHERE id = ?'
+  private static readonly UPDATE_QUERY =
+    'UPDATE subjects SET name = ?, welcome_text = ?, principal_id = ?, vice_principal_id = ?, country = ?, city=?, zip_code=?, street_address = ?, website = ? WHERE id = ?'
+  private static readonly DELETE_QUERY = 'DELETE FROM schools WHERE id = ?'
+
+  async create(entityWithoutId: Omit<V, 'id'>): Promise<K> {
+    return super.create(entityWithoutId, SchoolRepository.CREATE_QUERY)
+  }
+
+  async getAll(): Promise<V[]> {
+    return super.getAll(SchoolRepository.GET_ALL_QUERY)
+  }
+
+  async getById(id: K): Promise<V> {
+    return super.getById(id, SchoolRepository.GET_BY_ID)
+  }
+
+  async update(entityWithOutId: V, id: K): Promise<boolean> {
+    return super.update(entityWithOutId, id, SchoolRepository.UPDATE_QUERY)
+  }
+
+  async delete(id: K): Promise<boolean> {
+    return super.delete(id, SchoolRepository.DELETE_QUERY)
+  }
+}
+
+export default SchoolRepository
--- src/dao/shared-types.ts
+++ src/dao/shared-types.ts
@@ -1,6 +1,6 @@
 import { Identifiable } from '../model/shared-types'
 
-export interface IAPI<K, V extends Identifiable<K>> {
+export interface IRepository<K, V extends Identifiable<K>> {
   create(entityWithoutId: Omit<V, 'id'>, sqlQuery?: string): Promise<K>
   getAll(sqlQuery?: string): Promise<V[]>
   getById(id: K, sqlQuery?: string): Promise<V>
